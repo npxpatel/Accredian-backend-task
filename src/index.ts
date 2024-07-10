db033f09028e94e747c9d41503b742ae2802cd21
btrezzy <128296263+btrezzy@users.noreply.github.com>
2024-07-10 14:14:38 +0530

    Added frontend url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express"
-import cors from "cors" 
-import { PrismaClient } from '@prisma/client';
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -10,103 +10,71 @@ app.use(cors());
 app.use(express.json());
 
 dotenv.config();
-// app.use(cors({
-//   origin: 'https://wd-compiler-frontend.vercel.app', 
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'https://accredian-frontend-task-ashen-seven.vercel.app',
+  credentials: true,
+}));
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
-
 
 function isValidEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
-
 const prisma = new PrismaClient();
 
-app.post('/api/referrals', async (req: Request, res: Response) => {
-  const { name , email , refferedName, refferedEmail } = req.body;
-  
+app.post("/api/referrals", async (req: Request, res: Response) => {
+  const { name, email, refferedName, refferedEmail } = req.body;
 
   if (!name || !email || !refferedName || !refferedEmail) {
-    return res.status(400).json({ error: 'All fields are required' });
+    return res.status(400).json({ error: "All fields are required" });
   }
-  
+
   if (!isValidEmail(email) || !isValidEmail(refferedEmail)) {
-    return res.status(400).json({ error: 'Invalid email' });
+    return res.status(400).json({ error: "Invalid email" });
   }
 
   try {
-
-    
-
-        const referral = await prisma.referral.create({
-       data :{
-          Name : name,
-          Email : email,   
-          refferedName : refferedName,
-          refferedEmail : refferedEmail,
-       }
+    const referral = await prisma.referral.create({
+      data: {
+        Name: name,
+        Email: email,
+        refferedName: refferedName,
+        refferedEmail: refferedEmail,
+      },
     });
 
-
-    // 
+    //
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
-        user:  process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
-
 
     const mailOptions = {
-      from: process.env.EMAIL_USER ,
+      from: process.env.EMAIL_USER,
       to: refferedEmail,
-      subject: 'Referral from your friend',
-      text: `Hi ${name},\n\nYou have been referred by ${refferedName}.\n\nBest Regards,\nYour Company`
+      subject: "Referral from your friend",
+      text: `Hi ${name},\n\nYou have been referred by ${refferedName}.\n\nBest Regards,\nYour Company`,
     };
-  
 
-     const response = await transporter.sendMail(mailOptions);
-   
-      console.log(response)
+    const response = await transporter.sendMail(mailOptions);
+
+    console.log(response);
 
     res.status(201).json("ok");
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
 
 export default app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // const referral = await prisma.referral.create({
-    //    data :{
-    //       Name : name,
-    //       Email : email,   
-    //       refferedName : refferedName,
-    //       refferedEmail : refferedEmail,
-    //    }
-    // });
